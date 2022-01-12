cc1d9a1c512f817b132b3bf649245b13d141676a
amruthaakavaram <amruthaakavaram@gmail.com>
2022-01-12 22:15:54 +0530

    new file

---
+++ bitwiseoper.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    unsigned char a=45,b=34;
+    printf(" a&b=%d \n",a&b);
+    printf("a|b=%d \n",a|b);
+    printf(" a^b=%d \n",a^b);
+    printf(" ~a=%d \n",a=~a);
+    printf(" a<<1=%d \n",a<<1);
+    printf(" b>>1=%d \n",b>>1);
+    return 0;
+}
