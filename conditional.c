43bb244fa896c28da798375cd77b2f39110297bd
amruthaakavaram <97469951+amruthaakavaram@users.noreply.github.com>
2022-01-24 10:48:44 +0530

    new file

---
+++ conditional.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int r;
+    printf("enter the value of r:");
+    scanf("%d",&r);
+    if (r<=20)
+    {
+        printf("follow this route");
+    }
+    else
+    printf("follow another route");
+    return 0;
+}
