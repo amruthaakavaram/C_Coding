150c6421c4a676c2fcfdb323d2a75e258bdf979d
amruthaakavaram <97469951+amruthaakavaram@users.noreply.github.com>
2022-02-01 11:20:07 +0530

    '

---
+++ pyramid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    int a,b,n=9;
+    for(a=0; a<n; a++){
+           for(b=0; b<n+a; b++){
+               if(b<n-a-1)
+                   printf(" ");
+               else
+                    printf(".");
+               }
+        printf("\n");
+    }
+}
