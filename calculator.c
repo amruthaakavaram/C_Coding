f54f6d1d10edb7f23f9c8660dbf95e8d9d9ad0ac
amruthaakavaram <97469951+amruthaakavaram@users.noreply.github.com>
2022-02-03 14:02:13 +0530

    ,

---
+++ calculator.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i, operator;
+    char temp, temp1, c;
+    printf("enter two numbers:");
+    scanf("%d %d",&a,&b);
+    scanf("%c",&temp);
+    do
+    {
+    printf("1.Addition\n 2.Subtraction \n 3.Multiplication\n 4.Division\n 5.Modulus\n");
+    printf("enter the operator you want to perform:");
+    scanf("%d",&operator);
+    scanf("%c", temp1);
+    switch(operator)
+    {
+    case 1 :
+    i = a + b;
+    printf("addition of two numbers is %d\n",i);
+    break;
+
+    case 2 :
+    i = a - b;
+    printf("subtraction of two numbers is %d\n",i);
+    break;
+
+    case 3 :
+    i = a * b;
+    printf("multiplication of two numbers is %d\n",i);
+    break;
+
+    case 4 :
+    i = a / b;
+    printf("division of two numbers is %d\n",i);
+    break;
+
+    case 5 :
+    i = a % b;
+    printf("remainder is %d\n",i);
+    break;
+
+    default:
+    printf("ERROR: inavalid operator\n");
+    }
+    printf("Do you want continue the operation(y/Y):");
+    scanf("%c",&c);
+    }
+    while(c=='Y'||'y');
+    return 0;
+}
