d300d8fcba50fa1872026c14177c9f0088ca3deb
amruthaakavaram <97469951+amruthaakavaram@users.noreply.github.com>
2022-03-19 18:42:02 +0530

    .

--- transpose.c
+++ transpose.c
@@ -1,30 +1,23 @@
 #include<stdio.h>
 void main(){
-    int a[10][10],i,j,n,t;
-    printf("Enter no of rows:");
-    scanf("%d",&n);
-    printf("enter the elements of array:");
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n;j++)
-        {
+    int m,n,i,j,a[10][10],b[10][10];
+    printf("Enter no of rows & columns:");
+    scanf("%d%d",&m,&n);
+    printf("Enter the elements:");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
             scanf("%d",&a[i][j]);
         }
     }
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            if(i<j){
-                t=a[i][j];
-                a[i][j]=a[j][i];
-                a[j][i]=t;
-            }
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            b[j][i]=a[i][j];
         }
     }
-    for(i=0;i<n;i++){
+    printf("Transpose is:\n");
+    for(i=0;i<m;i++){
         for(j=0;j<n;j++){
-            printf("%d \t",a[i][j]);
+            printf("%d\t",b[i][j]);
         }
         printf("\n");
     }
