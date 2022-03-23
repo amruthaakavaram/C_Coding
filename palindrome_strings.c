d26fdffeaeb41d872e87b73a72206437ba30ed91
amruthaakavaram <97469951+amruthaakavaram@users.noreply.github.com>
2022-03-23 10:50:03 +0530

    .

---
+++ palindrome_strings.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<strings.h>
+void main(){
+    char a[100];
+    int j=0,length=0,temp=0;
+    printf("enter a string:");
+    scanf("%s",a);
+    printf("string length is %d\n",length);  
+}
